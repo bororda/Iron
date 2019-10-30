9f74f5be1f55fd4b078c8d4002a63b725625fcd7
bororda <vasilevichviktor@gmail.com>
2019-10-30 23:40:48 +0100

    tested and working on Wire.h; to be retested on attiny with tinyWireM.h

--- Iron.ino
+++ Iron.ino
@@ -1,59 +1,44 @@
-#include <Wire.h>
-//
-//#define pin_pwm 6
-//#define pin_ponent 2
-//#define pin_sensor 3
+#include <TinyWireM.h>
 
-//unsigned long lastTimeCheckedTemp = 0;
-//byte pwm_lvl = 0;
-//byte delta = 0;
+#define pin_pwm 6
+#define pin_ponent 2
+#define pin_sensor 3
 
-bool flag = 0;
+unsigned long lastTimeCheckedTemp = 0;
+byte pwm_lvl = 0;
+byte delta = 0;
 
 void setup() {
+  TinyWireM.begin();
   oledInit();
   oledClear();
+  pinMode(pin_pwm, OUTPUT);
+  //pinMode(pin_ponent, INPUT_PULLUP); //not needed
+  pinMode(pin_sensor, INPUT);
 }
 
-void loop() {
-  if (!flag) {
-    oledStatusUpdate();
-    flag = !flag;
+void loop()
+{
+  byte t_real = analogRead(pin_sensor);
+  byte t_target = analogRead(pin_ponent);
+  if ((millis() - lastTimeCheckedTemp) >= 2000)
+  {
+    if (t_target > t_real)
+    {
+      delta = t_target - t_real;
+      if (delta<16 & delta >= 8) pwm_lvl = 220;
+      else if (delta<8 & delta >= 4) pwm_lvl = 170;
+      else if (delta < 4) pwm_lvl = 120;
+      else pwm_lvl = 255;
+    }
+    else pwm_lvl = 0;
+
+    analogWrite(pin_pwm, pwm_lvl);
+
+    text();
+    digit(t_real, 0); digit(t_real, 1);
+    digit(pwm_lvl, 2); digit(pwm_lvl, 3);
+
+    lastTimeCheckedTemp = millis();
   }
 }
-
-//void setup() {
-//  pinMode(pin_pwm, OUTPUT);
-//  //pinMode(pin_ponent, INPUT_PULLUP) //not needed
-//  pinMode(pin_sensor, INPUT);
-//  Wire.begin();
-//  oledInit();
-//  oledClear();
-//}
-
-//void loop()
-//{
-//  byte t_real = analogRead(pin_sensor);
-//  byte t_target = analogRead(pin_ponent);
-//  if ((millis() - lastTimeCheckedTemp) >= 2000)
-//  {
-//    if (t_target > t_real)
-//    {
-//      delta = t_target - t_real;
-//      if (delta<16 & delta >= 8)
-//        pwm_lvl = 220;
-//      else if (delta<8 & delta >= 4)
-//        pwm_lvl = 170;
-//      else if (delta < 4)
-//        pwm_lvl = 120;
-//      else
-//        pwm_lvl = 255;
-//    }
-//    else
-//    {
-//      pwm_lvl = 0;
-//    }
-//    analogWrite(pin_pwm, pwm_lvl);
-//    lastTimeCheckedTemp = millis();
-//  }
-//}
--- oled.ino
+++ oled.ino
@@ -1,8 +1,8 @@
 #define OLED_ADDRESS            0x3C
 #define OLED_COMMAND_MODE       0x00
-//#define OLED_ONE_COMMAND_MODE   0x80
 #define OLED_DATA_MODE          0x40
-//#define OLED_ONE_DATA_MODE      0xC0
+#define COL_ADDRESS             0x21
+#define PAGE_ADDRESS            0x22
 
 const uint8_t _oled_init[] PROGMEM = {
   0xAE, // oled off
@@ -27,54 +27,80 @@ const uint8_t _oled_init[] PROGMEM = {
   0xAF,  // Display on*
 };
 
-const uint8_t CharMap[][6] PROGMEM = {
-  { 0x3E, 0x51, 0x49, 0x45, 0x3E, 0x00 }, // 30
-  { 0x00, 0x42, 0x7F, 0x40, 0x00, 0x00 },
-  { 0x72, 0x49, 0x49, 0x49, 0x46, 0x00 },
-  { 0x21, 0x41, 0x49, 0x4D, 0x33, 0x00 },
-  { 0x18, 0x14, 0x12, 0x7F, 0x10, 0x00 },
-  { 0x27, 0x45, 0x45, 0x45, 0x39, 0x00 },
-  { 0x3C, 0x4A, 0x49, 0x49, 0x31, 0x00 },
-  { 0x41, 0x21, 0x11, 0x09, 0x07, 0x00 },
-  { 0x36, 0x49, 0x49, 0x49, 0x36, 0x00 },
-  { 0x46, 0x49, 0x49, 0x29, 0x1E, 0x00 },
+const uint8_t textArea[][8] PROGMEM = {
+  {0x8E, 0x5B, 0x59, 0x98, 0x18, 0x19, 0x1B, 0x0E}, // c @ line 0
+  {0x01, 0x12, 0x32, 0x79, 0x30, 0x30, 0x34, 0x18}, // t @ line 1
+  {0xA2, 0xB6, 0xAA, 0xA2, 0xA2, 0xA2, 0xA2, 0xA2}, // WM @ line 2
+  {0xE8, 0x98, 0x98, 0x98, 0xE8, 0x8A, 0x8D, 0x88}, // PW @ line 3
 };
-
 void oledInit() {
-  Wire.beginTransmission(OLED_ADDRESS);
-  Wire.write(OLED_COMMAND_MODE);
-  for (uint8_t i = 0; i < 20; i++) Wire.write(pgm_read_byte(&_oled_init[i]));
-  Wire.endTransmission();
+  TinyWireM.beginTransmission(OLED_ADDRESS);
+  TinyWireM.send(OLED_COMMAND_MODE);
+  for (uint8_t i = 0; i < 20; i++) TinyWireM.send(pgm_read_byte(&_oled_init[i]));
+  TinyWireM.endTransmission();
 }
 
 void oledClear() {
-  Wire.beginTransmission(OLED_ADDRESS);
-  Wire.write(OLED_COMMAND_MODE);
-  Wire.write(0x21); Wire.write(0); Wire.write(127);
-  Wire.write(0x22); Wire.write(0); Wire.write(3);
-  Wire.endTransmission();
-  for (int i = 0; i < 32; i++) {
-    Wire.beginTransmission(OLED_ADDRESS);
-    Wire.write(OLED_DATA_MODE);
-    for (int i = 0; i < 32; i++) Wire.write(0);
-    Wire.endTransmission();
+  oledRange(9, 127, 0, 3);
+  for (uint8_t i = 0; i < 32; i++) {
+    TinyWireM.beginTransmission(OLED_ADDRESS);
+    TinyWireM.send(OLED_DATA_MODE);
+    for (uint8_t i = 0; i < 32; i++) TinyWireM.send(0);
+    TinyWireM.endTransmission();
   }
 }
 
-void oledStatusUpdate() {
-  Wire.beginTransmission(OLED_ADDRESS);
-  Wire.write(OLED_COMMAND_MODE);
-  // Set column address range
-  Wire.write(0x21); Wire.write(0); Wire.write(125); //TODO: unhardcode
-  // Set page address range
-  Wire.write(0x22); Wire.write(1); Wire.write(1); //TODO: unhardcode
-  Wire.endTransmission();
+void digit(uint8_t temperature, uint8_t line) { //value odd line 191, even line 253
+  uint8_t temp = temperature / 2;
+  if (temp < 9) {
+    temp = 9;
+  }
 
-  Wire.beginTransmission(OLED_ADDRESS);
-  Wire.write(OLED_DATA_MODE);
-  for (uint8_t col = 0 ; col < 6; col++) {
-    int bits = pgm_read_byte(&CharMap[0][col]); //TODO: unhardcode
-    Wire.write(bits);
-    Wire.endTransmission();
+  uint8_t valueF, valueE;
+  if (line == 0 || line == 2) {
+    valueF = 253;
+    valueE = 1;
+  }
+  else {
+    valueF = 191;
+    valueE = 128;
+  }
+
+  oledRange(9, 127, line, line);
+
+  for (uint8_t i = 9; i < 127; i++) {
+    TinyWireM.beginTransmission(OLED_ADDRESS);
+    TinyWireM.send(OLED_DATA_MODE);
+    for (uint8_t x = 0; x < 7; x++) {
+      if (i < temp) TinyWireM.send(valueF);
+      else TinyWireM.send(valueE);
+      i++;
+    }
+    i--;
+    TinyWireM.endTransmission();
   }
 }
+
+void text() {
+  for (uint8_t line = 0; line < 4; line++) {
+
+    oledRange(0, 7, line, line);
+
+    TinyWireM.beginTransmission(OLED_ADDRESS);
+    TinyWireM.send(OLED_DATA_MODE);
+    for (uint8_t x = 0; x < 8; x++) {
+      uint8_t bits = pgm_read_byte(&textArea[line][x]);
+      TinyWireM.send(bits);
+    }
+    TinyWireM.endTransmission();
+  }
+}
+
+void oledRange(uint8_t col1, uint8_t col2, uint8_t pg1, uint8_t pg2) {
+  TinyWireM.beginTransmission(OLED_ADDRESS);
+  TinyWireM.send(OLED_COMMAND_MODE);
+  TinyWireM.send(COL_ADDRESS); TinyWireM.send(col1); TinyWireM.send(col2); // Set column address range
+  TinyWireM.send(PAGE_ADDRESS); TinyWireM.send(pg1); TinyWireM.send(pg2); // Set page address range
+  TinyWireM.endTransmission();
+}
+
